43193fde9c7b0295b4bf3858ab0d551b114a4665
Sean Zhou <seantxzhou12@gmail.com>
2021-04-27 12:29:15 -0400

    setup vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1> Can you call for Beatrice as well as Kinzo can? </h1>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Karaoke/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Karaoke from './components/Karaoke.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Karaoke,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Karaoke.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>  
+        <img alt="Kinzo" src="../assets/kinzo.png">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Karaoke',
+  props: {
+  }
+}
+</script>
