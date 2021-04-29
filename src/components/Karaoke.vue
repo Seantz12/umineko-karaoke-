476ed1f1c1e392b7cd1dabf041aee0ddb004ab0d
Sean Zhou <seantxzhou12@gmail.com>
2021-04-28 20:28:58 -0400

    call api

--- src/components/Karaoke.vue
+++ src/components/Karaoke.vue
@@ -1,13 +1,43 @@
 <template>
-    <div>  
-        <img alt="Kinzo" src="../assets/kinzo.png">
-    </div>
+  <div>
+    <img alt="Kinzo" src="../assets/kinzo.png" />
+    <VueRecordAudio @result="onResult" />
+    <!--
+        <audio-recorder
+        upload-url="some url"
+        :attempts="3"
+        :time="2"
+        :before-recording="callback"
+        :after-recording="callback"
+        :before-upload="callback"
+        :successful-upload="callback"
+        :failed-upload="callback"/>
+        -->
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import FormData from 'form-data';
+
 export default {
-  name: 'Karaoke',
-  props: {
-  }
-}
+  name: "Karaoke",
+  components: {},
+  methods: {
+    callback(data) {
+      console.debug(data);
+    },
+    onResult(data) {
+      const path = "http://localhost:5000/compare";
+      const form = new FormData();
+      form.append('source', data);
+      form.append('compare', data);
+      axios.post(path, form).then(() => {
+          console.debug("did it");
+      });
+      console.debug(data);
+    },
+  },
+  props: {},
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRecord from '@codekraft-studio/vue-record'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRecord);
 
 new Vue({
   render: h => h(App),
